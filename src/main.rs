53e7a805611cfcc1599017c037c9c02374bdc301
Mylloon <kennel.anri@tutanota.com>
2023-09-03 13:16:12 +0200

    Use async in threads with tokio

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,29 @@ use std::{
     time::{Duration, Instant},
 };
 
-pub mod revanced;
+mod revanced;
 
-fn main() {
+#[actix_web::main]
+async fn main() {
     let scheduler = thread::spawn(|| {
         let wait_time = Duration::from_secs(60 * 60 * 12);
+
+        // Tokio runtime
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_io()
+            .enable_time()
+            .build()
+            .unwrap();
 
         loop {
             let start = Instant::now();
             eprintln!("Scheduler starting");
 
             // Prepare threads
-            let thread_a = thread::spawn(revanced::debug);
+            let thread_a = thread::spawn(revanced::search);
 
             // Run threads
-            thread_a.join().expect("Thread A panicked");
+            rt.block_on(async { thread_a.join().expect("Thread A panicked").await });
 
             let runtime = start.elapsed();
 
