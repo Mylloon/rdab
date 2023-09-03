543c57516f43a3e52abdaea106b5737c44f3b833
Mylloon <kennel.anri@tutanota.com>
2023-09-03 12:04:45 +0200

    add scheduler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
+
+pub mod revanced;
+
 fn main() {
-    println!("Hello, world!");
+    let scheduler = thread::spawn(|| {
+        let wait_time = Duration::from_secs(60 * 60 * 12);
+
+        loop {
+            let start = Instant::now();
+            eprintln!("Scheduler starting");
+
+            // Prepare threads
+            let thread_a = thread::spawn(revanced::debug);
+
+            // Run threads
+            thread_a.join().expect("Thread A panicked");
+
+            let runtime = start.elapsed();
+
+            if let Some(remaining) = wait_time.checked_sub(runtime) {
+                eprintln!(
+                    "Waiting for {} seconds before next run",
+                    remaining.as_secs()
+                );
+                thread::sleep(remaining);
+            }
+        }
+    });
+
+    // Run scheduler
+    scheduler.join().expect("Scheduler panicked");
 }
---
+++ src/revanced.rs
@@ -0,0 +1,4 @@
+pub fn debug() {
+    eprintln!("Hello, world!");
+    std::thread::sleep(std::time::Duration::from_millis(100))
+}
