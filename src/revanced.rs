9a3e8d7e3153e05ecf4d852aca9f15bf11dae402
Mylloon <kennel.anri@tutanota.com>
2023-09-03 13:16:29 +0200

    wip: find latest version

--- src/revanced.rs
+++ src/revanced.rs
@@ -1,4 +1,16 @@
-pub fn debug() {
-    eprintln!("Hello, world!");
-    std::thread::sleep(std::time::Duration::from_millis(100))
+use octocrab::models::repos::Release;
+
+pub async fn search() {
+    eprintln!("{:#?}", get_latest_version().await)
+}
+
+async fn get_latest_version() -> Release {
+    let octocrab = octocrab::instance();
+
+    octocrab
+        .repos("revanced", "revanced-patches")
+        .releases()
+        .get_latest()
+        .await
+        .expect("Can't find the latest version of Revanced")
 }
